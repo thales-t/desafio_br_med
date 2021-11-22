0967aea8701568ac86db0bc8d2a75674e05c0dfa
thales <thales.t@gmail.com>
2021-11-22 11:10:42 -0300

    configurando heroku

--- cotacoes/cotacoes/settings.py
+++ cotacoes/cotacoes/settings.py
@@ -129,3 +129,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
