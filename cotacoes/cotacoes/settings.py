e265ea24ecd432cf8fe0fde7efd00eee97272df5
Thales Evangelista <thales.t@gmail.com>
2021-11-22 19:37:04 -0300

    removendo secret key

--- cotacoes/cotacoes/settings.py
+++ cotacoes/cotacoes/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pq5^4i_1lglflj7g&^^7#si4#@*19dyqjp=122)h(vq49cw2d2'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = environ['DEBUG']
 
 ALLOWED_HOSTS = ['https://thales-desafio-br-med.herokuapp.com/']
 
